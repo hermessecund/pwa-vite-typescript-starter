de5871a0cf9d3cb56ea904032e82cd3831c41995
Tarwar <69816954+hermessecund@users.noreply.github.com>
2024-04-03 03:53:32 +0000

    Update Footer.tsx

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -1,13 +1,12 @@
 import React from "react";
-import { FaFacebook, FaTwitter, FaInstagram } from "react-icons/fa";
 
 const Footer = () => {
   return (
     <footer style={{ backgroundColor: "#333", color: "#fff", padding: "20px", position: "fixed", bottom: 0, left: 0, width: "100%", textAlign: "center" }}>
       <div>
-        <a href="#"><FaFacebook size={24} style={{ marginRight: "10px" }} /></a>
-        <a href="#"><FaTwitter size={24} style={{ marginRight: "10px" }} /></a>
-        <a href="#"><FaInstagram size={24} /></a>
+        <a href="#"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#fff" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M22 2h-20v20h20v-20z"/><path d="M15.5 9c0-1.381-1.119-2.5-2.5-2.5s-2.5 1.119-2.5 2.5 1.119 2.5 2.5 2.5c0.379 0 0.738-0.083 1.063-0.231 0.219 0.462 0.622 0.805 1.137 0.968-0.419 0.427-0.775 0.912-1.037 1.457-0.153 0.307-0.262 0.631-0.318 0.968h-2.444v3h2.427c0.083 0.314 0.214 0.612 0.419 0.888 0.472 0.715 1.221 1.219 2.081 1.303 0.08 0.007 0.159 0.011 0.239 0.011 1.381 0 2.5-1.119 2.5-2.5s-1.119-2.5-2.5-2.5h-1.5v-3h3.5c0.276 0 0.5-0.224 0.5-0.5v-1c0-0.276-0.224-0.5-0.5-0.5z"/></svg></a>
+        <a href="#"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#fff" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M22 2h-20v20h20v-20z"/><path d="M17.5 9c0-1.381-1.119-2.5-2.5-2.5s-2.5 1.119-2.5 2.5c0 0.668 0.256 1.279 0.673 1.736-0.218 0.354-0.365 0.769-0.418 1.214-0.594 0.055-1.065 0.548-1.065 1.155 0 0.655 0.532 1.187 1.187 1.187h1.705c0.783 0 1.418-0.635 1.418-1.418v-1.125c0-0.276-0.224-0.5-0.5-0.5h-1.902v-1.787c0-0.793 0.644-1.437 1.437-1.437h1.465c0.276 0 0.5-0.224 0.5-0.5v-1c0-0.276-0.224-0.5-0.5-0.5z"/></svg></a>
+        <a href="#"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#fff" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M22 2h-20v20h20v-20z"/><path d="M17 9.5c0-0.276-0.224-0.5-0.5-0.5h-7c-0.276 0-0.5 0.224-0.5 0.5s0.224 0.5 0.5 0.5h7c0.276 0 0.5-0.224 0.5-0.5z"/><path d="M17.5 12c0-1.933-1.567-3.5-3.5-3.5s-3.5 1.567-3.5 3.5c0 1.433 0.867 2.659 2.103 3.175-0.069 0.299-0.103 0.608-0.103 0.925 0 1.381 1.119 2.5 2.5 2.5s2.5-1.119 2.5-2.5c0-0.317-0.034-0.626-0.103-0.925 1.236-0.516 2.103-1.742 2.103-3.175z"/><circle cx="14.5" cy="12" r="1"/></svg></a>
       </div>
     </footer>
   );
