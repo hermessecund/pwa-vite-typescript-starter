cb3ab0030f517334daa727b6ea2c608f9f4a7b0a
Tarwar <69816954+hermessecund@users.noreply.github.com>
2024-04-03 05:45:24 +0000

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -89,16 +89,14 @@ export default function App() {
             </div>
           )}
 
-          <div className="map-container">
-            <iframe
-              src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d13225.474096078236!2d-6.7491399!3d34.0344167!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xda741c7c4865a09%3A0x28359510c8b5cf49!2sIBRAHIM%20CAR!5e0!3m2!1sen!2sma!4v1712114584631!5m2!1sen!2sma"
-              width="600"
-              height="450"
-              style={{ border: "2", borderRadius: "10px", background: "#f0f0f0", margin: "20px auto" }}
-              loading="lazy"
-              referrerPolicy="no-referrer-when-downgrade"
-            ></iframe>
-          </div>
+        <div className="map-container">
+  <iframe
+    src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d13225.474096078236!2d-6.7491399!3d34.0344167!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xda741c7c4865a09%3A0x28359510c8b5cf49!2sIBRAHIM%20CAR!5e0!3m2!1sen!2sma!4v1712114584631!5m2!1sen!2sma"
+    style={{ border: "2px solid", borderRadius: "10px", background: "#f0f0f0", margin: "20px auto", width: "100%", maxWidth: "600px" }}
+    loading="lazy"
+    referrerPolicy="no-referrer-when-downgrade"
+  ></iframe>
+</div>
         </div>
       </div>
       <Footer />
