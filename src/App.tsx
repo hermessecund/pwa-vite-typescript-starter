466fc06ec25d978eef8fa9f3f4128c173aa77d6f
Tarwar <69816954+hermessecund@users.noreply.github.com>
2024-04-03 02:41:17 +0000

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,96 +1,94 @@
-import {
-  ThirdwebProvider,
-  ConnectWallet,
-  metamaskWallet,
-  coinbaseWallet,
-  walletConnect,
-  embeddedWallet,
-  trustWallet,
-  rainbowWallet,
-  phantomWallet,
-  lightTheme,
-} from "@thirdweb-dev/react";
+import { ConnectWallet } from "@thirdweb-dev/react";
 import "./styles/Home.css";
-
-const smartWalletOptions = {
-  factoryAddress: "0x83eE9EF2CE5072F44FD14bf575CA6bB2D704F734",
-  gasless: true,
-};
 
 export default function Home() {
   return (
-    <ThirdwebProvider
-      activeChain="mumbai"
-      clientId="ecdb63b562523a1c1cf3fe1bbdc33ad0"
-      locale={en()}
-      supportedWallets={[
-        smartWallet(
-          metamaskWallet(),
-          smartWalletOptions,
-        ),
-        smartWallet(
-          coinbaseWallet({ recommended: true }),
-          smartWalletOptions,
-        ),
-        smartWallet(
-          walletConnect(),
-          smartWalletOptions,
-        ),
-        smartWallet(
-          embeddedWallet({
-            auth: {
-              options: [
-                "email",
-                "google",
-                "apple",
-                "facebook",
-              ],
-            },
-          }),
-          smartWalletOptions,
-        ),
-        smartWallet(
-          trustWallet(),
-          smartWalletOptions,
-        ),
-        smartWallet(
-          rainbowWallet(),
-          smartWalletOptions,
-        ),
-        smartWallet(
-          phantomWallet(),
-          smartWalletOptions,
-        ),
-      ]}
-      authConfig={{
-        authUrl: "/api/auth",
-        domain: "https://example.com",
-      }}
-    >
-      <ConnectWallet
-        theme={lightTheme({
-          colors: {
-            primaryButtonBg: "#280764",
-            primaryText: "#18043e",
-          },
-        })}
-        modalTitle={"TiFi Market"}
-        auth={{ loginOptional: false }}
-        switchToActiveChain={true}
-        modalSize={"wide"}
-        welcomeScreen={{
-          subtitle: "",
-          img: {
-            src: "https://tarwar.com/tifi/tf.png",
-            width: 150,
-            height: 150,
-          },
-        }}
-        modalTitleIconUrl={
-          "https://tarwar.com/tifi/img/tifi.png"
-        }
-      />
-    </ThirdwebProvider>
+    <main className="main">
+      <div className="container">
+        <div className="header">
+          <h1 className="title">
+            Welcome to{" "}
+            <span className="gradient-text-0">
+              <a
+                href="https://thirdweb.com/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                thirdweb.
+              </a>
+            </span>
+          </h1>
+
+          <p className="description">
+            Get started by configuring your desired network in{" "}
+            <code className="code">src/index.js</code>, then modify the{" "}
+            <code className="code">src/App.js</code> file!
+          </p>
+
+          <div className="connect">
+            <ConnectWallet />
+          </div>
+        </div>
+
+        <div className="grid">
+          <a
+            href="https://portal.thirdweb.com/"
+            className="card"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              src="/images/portal-preview.png"
+              alt="Placeholder preview of starter"
+            />
+            <div className="card-text">
+              <h2 className="gradient-text-1">Portal ➜</h2>
+              <p>
+                Guides, references, and resources that will help you build with
+                thirdweb.
+              </p>
+            </div>
+          </a>
+
+          <a
+            href="https://thirdweb.com/dashboard"
+            className="card"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              src="/images/dashboard-preview.png"
+              alt="Placeholder preview of starter"
+            />
+            <div className="card-text">
+              <h2 className="gradient-text-2">Dashboard ➜</h2>
+              <p>
+                Deploy, configure, and manage your smart contracts from the
+                dashboard.
+              </p>
+            </div>
+          </a>
+
+          <a
+            href="https://thirdweb.com/templates"
+            className="card"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              src="/images/templates-preview.png"
+              alt="Placeholder preview of templates"
+            />
+            <div className="card-text">
+              <h2 className="gradient-text-3">Templates ➜</h2>
+              <p>
+                Discover and clone template projects showcasing thirdweb
+                features.
+              </p>
+            </div>
+          </a>
+        </div>
+      </div>
+    </main>
   );
 }
-
