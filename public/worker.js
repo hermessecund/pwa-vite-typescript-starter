a0a557de7c471cdc18503b49d5b0223c4c08f461
Tarwar <69816954+hermessecund@users.noreply.github.com>
2024-04-03 03:43:57 +0000

    Update worker.js

--- public/worker.js
+++ public/worker.js
@@ -1,4 +1,4 @@
-var CACHE_NAME = "pwa-task-manager";
+var CACHE_NAME = "ibcar-manager";
 var urlsToCache = ["/", "/completed"];
 
 // Install a service worker
@@ -27,7 +27,7 @@ self.addEventListener("fetch", (event) => {
 
 // Update a service worker
 self.addEventListener("activate", (event) => {
-  var cacheWhitelist = ["pwa-task-manager"];
+  var cacheWhitelist = ["ibcar-manager"];
   event.waitUntil(
     caches.keys().then((cacheNames) => {
       return Promise.all(
