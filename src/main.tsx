de652f9fed5c492e5ac1be894f2f3fddafc3ad6b
Tarwar <69816954+hermessecund@users.noreply.github.com>
2024-04-03 03:28:48 +0000

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -86,9 +86,15 @@ export default function App() {
             </div>
           )}
           <div className="map-container">
-        <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d13225.474096078236!2d-6.7491399!3d34.0344167!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xda741c7c4865a09%3A0x28359510c8b5cf49!2sIBRAHIM%20CAR!5e0!3m2!1sen!2sma!4v1712114584631!5m2!1sen!2sma" width="600" height="450" style={{border: "0", borderRadius: "10px", background: "#f0f0f0", margin: "20px auto"}} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-      </div>
-        </div>
+       <iframe
+  src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d13225.474096078236!2d-6.7491399!3d34.0344167!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xda741c7c4865a09%3A0x28359510c8b5cf49!2sIBRAHIM%20CAR!5e0!3m2!1sen!2sma!4v1712114584631!5m2!1sen!2sma"
+  width="600"
+  height="450"
+  style={{ border: "0", borderRadius: "10px", background: "#f0f0f0", margin: "20px auto" }}
+  allowFullScreen="" // Change allowfullscreen to allowFullScreen
+  loading="lazy"
+  referrerPolicy="no-referrer-when-downgrade"
+></iframe>
       </div>
     </main>
   ) : (
