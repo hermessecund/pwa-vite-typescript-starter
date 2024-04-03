afa78e4383836cbbdfdb8c2ea930518bc7ad2c75
Tarwar <69816954+hermessecund@users.noreply.github.com>
2024-04-03 05:35:25 +0000

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,38 @@
 import { ConnectWallet } from "@thirdweb-dev/react";
-import "./styles/Home.css";
+import { Link } from "react-router-dom";
+import "./styles/main.css";
+
+// Path to logo and background image
+const logoPath = "/images/logo.png";
+const backgroundImagePath = "/images/31.png";
 
 export default function Home() {
   return (
-    <main className="main">      
-      <div className="connect">
-            <ConnectWallet />
-      </div>
+    <main className="main" style={{ backgroundImage: `url(${backgroundImagePath})` }}>
+      {/* Navigation Bar */}
+      <nav className="navbar">
+        {/* Logo */}
+        <div className="logo">
+          <Link to="/">
+            <img src={logoPath} alt="Logo" />
+          </Link>
+        </div>
+        {/* Navigation Links */}
+        <ul className="nav-links">
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">About</Link></li>
+          <li><Link to="/contact">Contact</Link></li>
+        </ul>
+        {/* Connect Button */}
+        <div className="connect">
+          <ConnectWallet />
+        </div>
+      </nav>
+      
       <div className="container">
         <div className="header">
-          <h1 className="title">         
+          <h1 className="title">
+            {/* Title Content */}
           </h1>
           <p className="description">
             Get started
